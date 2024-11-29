2460ed584a27e4d13c08aec80a478b2626a6d394
Kartik <87748398+kartik5026@users.noreply.github.com>
2024-11-29 15:43:03 +0530

    Update server.js

--- back-end/server.js
+++ back-end/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cookieParser());
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://assignment-hr-dashboard.vercel.app/',
     credentials:true
 
 }))
